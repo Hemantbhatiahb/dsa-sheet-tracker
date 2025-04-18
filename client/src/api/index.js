0111f82b7fb96973a554be8f6989d7fd46b74cd8
Hemant Bhatia <hemant@Hemants-MacBook-Air.local>
2025-04-18 15:40:18 +0530

    updated index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://dsa-sheet-tracker.onrender.com",
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
